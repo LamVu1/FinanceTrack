3068b766bdb0f21cd3d542d8bcc1aacc472a237c
LamVu1 <lvu2548@gmail.com>
2020-04-30 17:35:25 -0700

    Line Graph updated

--- frontend/src/components/linegraph/line_graph_component.jsx
+++ frontend/src/components/linegraph/line_graph_component.jsx
@@ -7,22 +7,30 @@ import "./line_graph_component.css"
 
 import { useSelector } from 'react-redux'
 import {transactionSelector} from '../bargraph/transaction_selectors';
-// [25,30,45,60,28,75]
+
 export const LineGraph =()=>{
     let data = useSelector(transactionSelector)
     
-    
-
-    data.forEach(d=>{
-d.amount = +d.amount;
-d.date = new Date(d.date)
-    })
+    let min = 0;
+    let runningBalance3 = 0;
 
     data.sort((a,b)=>new Date(a.date) - new Date(b.date))
-    var margin = {top: 25, right: 25, bottom: 25, left: 25};
 
-    var width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+    data.forEach(d=>{
+        d.amount = +d.amount;
+        d.date = new Date(d.date);
+        runningBalance3+=d.amount;
+        min = Math.min(min, runningBalance3);
+    })
+
+    let runningBalance = 0;
+    let runningBalance2 = 0;
+
+
+    var margin = {top: 35, right: 35, bottom: 25, left: 35};
+
+    var width = 800 - margin.left - margin.right,
+        height = 600 - margin.top - margin.bottom;
     
     // var svg = d3.select(element[0])
     //             .append('svg')
@@ -37,16 +45,25 @@ d.date = new Date(d.date)
     // const svgRef = useRef();
     // useEffect(()=>{
         // console.log(data)
-        const svg = d3.select('svg').attr('width', width+margin)
-                    .attr('height', height+margin);
+        const svg = d3.select('svg').attr('width', width+margin.left+margin.right)
+                    .attr('height', height+margin.top+margin.bottom);
                     svg.append('g')
                     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
         
 
         const xValue = d =>{ 
             return(
-            new Date(d.date)
+            (d.date)
             )}
+
+
+        const yValue = d =>{
+
+            return(
+                runningBalance+= d.amount
+
+            )
+        }    
         // const yValue = d => {
         //     return(
         //     +d.amount)};
@@ -55,32 +72,67 @@ d.date = new Date(d.date)
         //X axis
         const xScale = d3.scaleTime()
                         .domain(d3.extent(data, function(d){return d.date}))
-                        .range([0,width])
+                        .range([60,width])
 
                 // svg.append("g").attr("transform", "translate(0,"+'500px'+")").call(d3.axisBottom(xScale));
 
         const xAxis = d3.axisBottom(xScale)
         // .ticks(data.length).tickFormat(index=>index+1)
        
-        svg.select(".x-axis").style("transform","translate(60px,520px)").call(xAxis);
-
+        
+        
+        
         
         //Y axis
         const yScale = d3.scaleLinear()
-        .domain([0, d3.max(data, function(d){return +d.amount})])
-        .range([height,0]);
+        .domain([min, d3.max(data, function(d){return runningBalance2+=d.amount})])
+        .range([height-20,20]);
+        
+
+        // console.log(yScale(0))
+
+
+        svg.select(".x-axis").style("transform","translate(0px,"+  yScale(0)+ 'px)').call(xAxis);
+
+        
 
         const yAxis = d3.axisLeft(yScale);
 
-        svg.select(".y-axis").text('Amount').style("transform","translate(60px,20px)").call(yAxis);
-                    
+        svg.select(".y-axis").text('Amount').style("transform","translate(60px,0px)").call(yAxis);
+     
+        svg.append("g")
+        .append("text")
+        .attr("y", 560)
+        .attr("x", width/2)
+        .attr("dy", ".71em")
+        .style("font-size", "10px")
+        .text("Date");
+
+
+        svg.append("g")
+        .append("text")
+        .attr("y", 20)
+        .attr("x", width/2)
+        .attr("dy", ".71em")
+        .style("font-size", "10px")
+        .text("Balance");
+
+        // .attr("y", height/2)
+        // .attr("x", width/2)
+
+        svg.append("g")
+        .append("text")
+        .attr("dy", ".71em")
+        .style("font-size", "10px")
+        .attr( "transform", "translate(20,300) rotate(-90)")
+        .text("Balance Amount in Dollars");
 
         //Line
         const myLine = d3.line()
                 .x(data=> 
                     xScale(xValue(data)))
                 .y(data=> 
-                    yScale(data.amount))
+                    yScale(yValue(data)))
                 // .curve(d3.curveCardinal); 
     
         svg.selectAll(".line")
@@ -89,8 +141,14 @@ d.date = new Date(d.date)
             .attr("class","line")
             .attr("d", myLine(data))
             .attr("fill","none")
-            .attr("stroke", "blue")
+            .attr("stroke", "#896ccc")
     // }, [data]);
+
+
+
+
+
+
 
     // ref={svgRef}
       return (
