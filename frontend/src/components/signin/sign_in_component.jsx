778e443c57f275ffa0a3b0c59ec2f055807fb8de
LamVu1 <lvu2548@gmail.com>
2020-05-13 12:28:32 -0700

    Changed class components to functional with react hooks

--- frontend/src/components/signin/sign_in_component.jsx
+++ frontend/src/components/signin/sign_in_component.jsx
@@ -1,106 +1,174 @@
 import { connect } from 'react-redux';
 import { login } from '../../reducers/session/session_actions';
-import React from 'react';
+import React, {useState} from 'react';
 import { withRouter } from 'react-router-dom';
 import './sign_in_component.css'
 
 
-class SignIn extends React.Component {
-  constructor(props) {
-    super(props);
+// class SignIn extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      username: '',
-      password: '',
-      errors: {}
-    };
+//     this.state = {
+//       username: '',
+//       password: '',
+//       errors: {}
+//     };
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.renderErrors = this.renderErrors.bind(this);
-    this.handleDemo = this.handleDemo.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//     this.renderErrors = this.renderErrors.bind(this);
+//     this.handleDemo = this.handleDemo.bind(this);
 
-  }
+//   }
 
-  // Once the user has been authenticated, redirect to the Tweets page
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.currentUser === true) {
-      this.props.history.push('/main');
+//   // Once the user has been authenticated, redirect to the Tweets page
+//   componentWillReceiveProps(nextProps) {
+//     if (nextProps.currentUser === true) {
+//       this.props.history.push('/main');
+//     }
+
+//     // Set or clear errors
+//     this.setState({errors: nextProps.errors})
+//   }
+
+//   // Handle field updates (called in the render method)
+//   update(field) {
+//     return e => this.setState({
+//       [field]: e.currentTarget.value
+//     });
+//   }
+
+//   handleDemo(e){
+//     e.preventDefault();
+//     let user = {
+//       username: 'test1',
+//       password: '123456'
+//     };
+//     this.props.login(user); 
+
+//   }
+
+//   // Handle form submission
+//   handleSubmit(e) {
+//     e.preventDefault();
+
+//     let user = {
+//       username: this.state.username,
+//       password: this.state.password
+//     };
+
+//     this.props.login(user); 
+//   }
+
+//   // Render the session errors if there are any
+//   renderErrors() {
+//     return(
+//       <ul>
+//         {Object.keys(this.state.errors).map((error, i) => (
+//           <li key={`error-${i}`}>
+//             {this.state.errors[error]}
+//           </li>
+//         ))}
+//       </ul>
+//     );
+//   }
+
+//   render() {
+//     return (
+//       <div className='sign-in'>
+//         <h1>Sign In</h1>
+//         <form  className='sign-in-form'>
+       
+//               <input type="text"
+//                 value={this.state.username}
+//                 onChange={this.update('username')}
+//                 placeholder="Username"
+//               />
+//               <input type="password"
+//                 value={this.state.password}
+//                 onChange={this.update('password')}
+//                 placeholder="Password"
+//               />
+//           <button className='login-in-btn' onClick={this.handleSubmit} >Submit</button>
+//           <button className='demo-login' onClick={this.handleDemo}>Demo Log In</button>
+//             {this.renderErrors()}
+          
+//         </form>
+//         <div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+
+const SignIn = ({login})=>{
+
+  let [username, setUserName] = useState('');
+  let [password, setPassword] = useState('');
+
+  const update = (e)=>{
+    let { name, value } = e.target;
+    if(name==='username'){
+      setUserName(value)
     }
+    if(name==='password'){
+      setPassword(value)
+    }
+  }    
+  
 
-    // Set or clear errors
-    this.setState({errors: nextProps.errors})
-  }
-
-  // Handle field updates (called in the render method)
-  update(field) {
-    return e => this.setState({
-      [field]: e.currentTarget.value
-    });
-  }
-
-  handleDemo(e){
+  const handleDemo =(e)=>{
     e.preventDefault();
     let user = {
       username: 'test1',
       password: '123456'
     };
-    this.props.login(user); 
-
+    login(user); 
   }
 
-  // Handle form submission
-  handleSubmit(e) {
+
+  const handleSubmit=(e)=>{
     e.preventDefault();
 
     let user = {
-      username: this.state.username,
-      password: this.state.password
+      username: username,
+      password: password
     };
 
-    this.props.login(user); 
+    login(user); 
   }
 
-  // Render the session errors if there are any
-  renderErrors() {
-    return(
-      <ul>
-        {Object.keys(this.state.errors).map((error, i) => (
-          <li key={`error-${i}`}>
-            {this.state.errors[error]}
-          </li>
-        ))}
-      </ul>
-    );
-  }
 
-  render() {
-    return (
-      <div className='sign-in'>
-        <h1>Sign In</h1>
-        <form  className='sign-in-form'>
-       
-              <input type="text"
-                value={this.state.username}
-                onChange={this.update('username')}
-                placeholder="Username"
-              />
-              <input type="password"
-                value={this.state.password}
-                onChange={this.update('password')}
-                placeholder="Password"
-              />
-          <button className='login-in-btn' onClick={this.handleSubmit} >Submit</button>
-          <button className='demo-login' onClick={this.handleDemo}>Demo Log In</button>
-            {this.renderErrors()}
-          
-        </form>
-        <div>
-        </div>
-      </div>
-    );
-  }
+  //   renderErrors() {
+//     return(
+//       <ul>
+//         {Object.keys(this.state.errors).map((error, i) => (
+//           <li key={`error-${i}`}>
+//             {this.state.errors[error]}
+//           </li>
+//         ))}
+//       </ul>
+//     );
+//   }
+
+
+  return (
+    <div className='sign-in'>
+      <h1>Sign In</h1>
+      
+      <form  className='sign-in-form'>
+      
+            <input type="text" value={username} name='username' onChange={update} placeholder="Username"/>
+            <input type="password" value={password} name='password' onChange={update} placeholder="Password"/>
+            <button className='login-in-btn' onClick={handleSubmit} >Submit</button>
+            <button className='demo-login' onClick={handleDemo}>Demo Log In</button>
+        
+      </form>
+     
+    </div>
+  );
 }
-
 
 const mapStateToProps = (state) => {
   return {
@@ -114,7 +182,4 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-export default withRouter(connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(SignIn));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SignIn));
--- frontend/src/components/transaction_form/transaction_form_component.jsx
+++ frontend/src/components/transaction_form/transaction_form_component.jsx
@@ -1,55 +1,160 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { connect } from 'react-redux';
 
 import {createTransaction} from '../../reducers/transactions/transaction_actions';
 import './transaction_form_component.css';
 
-class TransactionForm extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            item: '',
-            amount: '',
-            date: '',
-            toggle: true
-        };
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleUpdate = this.handleUpdate.bind(this);
-        this.handleToggle = this.handleToggle.bind(this);
-        this.handleDate = this.handleDate.bind(this);
-    }
+// class TransactionForm extends React.Component{
+//     constructor(){
+//         super();
+//         this.state = {
+//             item: '',
+//             amount: '',
+//             date: '',
+//             toggle: true
+//         };
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//         this.handleUpdate = this.handleUpdate.bind(this);
+//         this.handleToggle = this.handleToggle.bind(this);
+//         this.handleDate = this.handleDate.bind(this);
+//     }
 
-    handleUpdate(e){
+//     handleUpdate(e){
+//         let { name, value } = e.target;
+
+//         //convert user input to positive
+//         if(name==='amount'&& value!==''){
+//             value = Math.abs(value)
+//         }
+//         this.setState({ [name]: value });
+//     }
+
+//     handleDate(e){
+//         this.setState({date: e.target.value})
+//     }
+
+//     handleSubmit(e){
+//        e.preventDefault();
+//        let newAmount = this.state.amount
+//        if(!this.state.toggle){
+//          newAmount = newAmount*-1
+//        }
+//        const user = Object.assign({}, {...this.state});
+//        user.amount = newAmount
+//        delete user.toggle;
+//         if(this.state.date===''){
+//             delete user.date
+//         }
+//         this.setState({item: '', amount:'', date: ''})
+//        this.props.createTransaction(user)
+//      }
+
+//      handleToggle(e){
+         
+//         // e.target.classList.add('selected')
+        
+//         if(e.target.classList.contains('selected')){
+//             return
+//         }
+//         // if(e.target.innerText==='Income'){
+//         //     let btn = document.getElementsByClassName('toggle-spending');
+//         //     btn[0].classList.remove('selected');
+//         //     e.target.classList.add('selected');
+//         //     this.toggle= true;
+//         // }
+//         // if(e.target.innerText==='Spending'){
+//         //     let btn = document.getElementsByClassName('toggle-income');
+//         //     btn[0].classList.remove('selected');
+//         //     e.target.classList.add('selected');
+//             if(this.state.toggle){
+//                this.setState({toggle: false})
+//             }else{
+//                 this.setState({toggle: true})
+//             }
+//      }
+    
+
+//     render(){
+
+//         const {item, amount, date} = this.state;
+
+//         return(
+//             <div className='transaction-form-container'>
+//                 <form action="" className='transaction-form'>
+
+//                     <h1 className='transaction-form-header'>Add new transaction</h1>
+//                    <div className='form-toggle'>
+//                         <button className={`toggle-income ${this.state.toggle===true ?'selected' :''}` } onClick={(e)=>{this.handleToggle(e)}}>
+//                             Income
+//                         </button>
+//                         <button className={`toggle-spending ${this.state.toggle===false ?'selected' :''}`} onClick={(e)=>{this.handleToggle(e)}}>
+//                             Expense
+//                         </button>
+//                    </div>
+//                     <div className='form-item'>
+//                         <label className='item-label'>Date (If obmitted will default to current date UTC)</label>
+//                         <input className='item-input-date' type="date" name="" id="" value={date} title='Default current date' onChange={this.handleDate}/>
+//                         <label className='item-label' htmlFor="">Item</label>
+//                             <input className='item-input' type="text" name='item' maxLength={30} value={item} onChange={this.handleUpdate}/>
+//                         <label htmlFor="" className='item-label' >Amount</label>
+//                             <input className='item-input' type="number" name='amount' placeholder='Enter Amount' min="0" value={amount} onChange={this.handleUpdate}/>
+//                     </div>
+//                     <button className='transaction-form-submit-btn' onClick={this.handleSubmit}>
+//                         Add transaction
+//                     </button>      
+
+//                 </form>
+//             </div>
+//         )
+//     }
+// }
+
+
+
+const TransactionForm = ({createTransaction})=>{
+    let [item, setItem] = useState('');
+    let [amount, setAmount] = useState('');
+    let [date, setDate] = useState('');
+    let [toggle, setToggle] = useState(true);
+
+    const handleUpdate =(e)=>{
         let { name, value } = e.target;
 
+        if(name==='item'){
+            setItem(value)
+        }
         //convert user input to positive
         if(name==='amount'&& value!==''){
             value = Math.abs(value)
+            setAmount(value)
         }
-        this.setState({ [name]: value });
     }
 
-    handleDate(e){
-        this.setState({date: e.target.value})
+    const handleDate=(e)=>{
+        setDate(e.target.value)
     }
 
-    handleSubmit(e){
+    const handleSubmit=(e)=>{
        e.preventDefault();
-       let newAmount = this.state.amount
-       if(!this.state.toggle){
+       let newAmount = amount;
+
+       if(!toggle){
          newAmount = newAmount*-1
        }
-       const user = Object.assign({}, {...this.state});
-       user.amount = newAmount
-       delete user.toggle;
-        if(this.state.date===''){
-            delete user.date
+
+       const user = {item: item, amount: newAmount}
+
+        if(date !==''){
+          user.date = date
         }
-        this.setState({item: '', amount:'', date: ''})
-       this.props.createTransaction(user)
+
+        setItem('');
+        setAmount('');
+        setDate('');
+        createTransaction(user)
      }
 
-     handleToggle(e){
+    const handleToggle=(e)=>{
          
         // e.target.classList.add('selected')
         
@@ -66,53 +171,49 @@ class TransactionForm extends React.Component{
         //     let btn = document.getElementsByClassName('toggle-income');
         //     btn[0].classList.remove('selected');
         //     e.target.classList.add('selected');
-            if(this.state.toggle){
-               this.setState({toggle: false})
-            }else{
-                this.setState({toggle: true})
-            }
+        setToggle(!toggle);
      }
-    
 
-    render(){
+    return(
+        <div className='transaction-form-container'>
+            <form action="" className='transaction-form'>
 
-        const {item, amount, date} = this.state;
+                <h1 className='transaction-form-header'>Add new transaction</h1>
 
-        return(
-            <div className='transaction-form-container'>
-                <form action="" className='transaction-form'>
+                <div className='form-toggle'>
+                    <button className={`toggle-income ${toggle===true ?'selected' :''}` } onClick={handleToggle}>
+                        Income
+                    </button>
+                    <button className={`toggle-spending ${toggle===false ?'selected' :''}`} onClick={handleToggle}>
+                        Expense
+                    </button>
+                </div>
 
-                    <h1 className='transaction-form-header'>Add new transaction</h1>
-                   <div className='form-toggle'>
-                        <button className={`toggle-income ${this.state.toggle===true ?'selected' :''}` } onClick={(e)=>{this.handleToggle(e)}}>
-                            Income
-                        </button>
-                        <button className={`toggle-spending ${this.state.toggle===false ?'selected' :''}`} onClick={(e)=>{this.handleToggle(e)}}>
-                            Expense
-                        </button>
-                   </div>
-                    <div className='form-item'>
-                        <label className='item-label'>Date (If obmitted will default to current date UTC)</label>
-                        <input className='item-input-date' type="date" name="" id="" value={date} title='Default current date' onChange={this.handleDate}/>
-                        <label className='item-label' htmlFor="">Item</label>
-                            <input className='item-input' type="text" name='item' maxLength={30} value={item} onChange={this.handleUpdate}/>
-                        <label htmlFor="" className='item-label' >Amount</label>
-                            <input className='item-input' type="number" name='amount' placeholder='Enter Amount' min="0" value={amount} onChange={this.handleUpdate}/>
-                    </div>
-                    <button className='transaction-form-submit-btn' onClick={this.handleSubmit}>
-                        Add transaction
-                    </button>      
+                <div className='form-item'>
+                    <label className='item-label'>Date (If obmitted will default to current date UTC)</label>
+                        <input className='item-input-date' type="date" name="" id="" value={date} title='Default current date' onChange={handleDate}/>
+                    <label className='item-label' htmlFor="">Item</label>
+                        <input className='item-input' type="text" name='item' maxLength={30} value={item} onChange={handleUpdate}/>
+                    <label htmlFor="" className='item-label' >Amount</label>
+                        <input className='item-input' type="number" name='amount' placeholder='Enter Amount' min="0" value={amount} onChange={handleUpdate}/>
+                </div>
+                
+                <button className='transaction-form-submit-btn' onClick={handleSubmit}>
+                    Add transaction
+                </button>      
 
-                </form>
-            </div>
-        )
-    }
+            </form>
+        </div>
+    )
+
 }
 
 
 const mapStateToProps =(dispatch)=>{
+
     return(
-        {createTransaction: (data)=>dispatch(createTransaction(data))
+        {
+            createTransaction: (data)=>dispatch(createTransaction(data))
     
         }
     )
